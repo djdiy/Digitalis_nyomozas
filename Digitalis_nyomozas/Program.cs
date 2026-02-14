d12cabd0159466b06d1afe73013630514dc736b0
karas <karas@KARASZ>
2026-02-14 13:49:37 +0100

    Add project files.

---
+++ Digitalis_nyomozas/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Digitalis_nyomozas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
