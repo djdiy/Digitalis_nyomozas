4a16bce5c61ef019c85295c8627ca8a49e7f3e1a
karasz <zsombfish@gmail.com>
2026-02-14 18:56:09 +0100

    Evidence class kesz

---
+++ Digitalis_nyomozas/Evidence.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Digitalis_nyomozas
+{
+    internal class Evidence
+    {
+        private int id;
+        private string type;
+        private string description;
+        private int trustworthiness;
+
+        public Evidence(int id, string type, string description, int trustworthiness)
+        {
+            this.id = id;
+            this.type = type;
+            this.description = description;
+            this.trustworthiness = trustworthiness;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Type { get => type; set => type = value; }
+        public string Description { get => description; set => description = value; }
+        public int Trustworthiness { get => trustworthiness; set => trustworthiness = value; }
+
+
+
+        public override string ToString()
+        {
+            return $"Azonosító: {this.id}, típus: {this.type}, leírás: {this.description}, megbízhatósági érték(1-5): {this.trustworthiness}";
+        }
+    
+    
+    }
+}
--- Digitalis_nyomozas/Program.cs
+++ Digitalis_nyomozas/Program.cs
@@ -4,21 +4,36 @@
     {
         static void Main(string[] args)
         {
+            //Felhasználó teszt
             User user_test = new User("Test_user", 0, "testing");
             Console.WriteLine(user_test);
 
 
+            //Személy teszt
             Person person_test = new Person("Test_person", 123, "123 éves");
             Console.WriteLine(person_test);
 
-            Person suspect_person_test = new Person("suspected_person", 321, "321 éves és gyanusított");
 
+
+            //Gyanusított teszt
+            Person suspect_person_test = new Person("suspected_person", 321, "321 éves és gyanusított");
             Suspect suspect_test = new Suspect(suspect_person_test, 99, "őrizetben");
             Console.WriteLine(suspect_test);
 
+
+
+
+            //Tanú teszt
             Person witness_person_test = new Person("witness_person", 1, "1 éves és tanú");
             Witness witness_test = new Witness(witness_person_test, "láttam", DateOnly.FromDateTime(DateTime.Now));
             Console.WriteLine(witness_test);
+
+
+
+
+            //Bizonyíték teszt
+            Evidence evidence_test = new Evidence(1, "fotó", "wow, ez nagyon biztos", 5);
+            Console.WriteLine(evidence_test);
 
 
 
